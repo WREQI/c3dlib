7beb2c662063c79d2637dee0a4d7c889a6c1a1f3
asdass <35467304+WREQI@users.noreply.github.com>
2026-08-11 00:33:11 +0800

    1
    1

--- src/views/ArchitectureView.vue
+++ src/views/ArchitectureView.vue
@@ -48,14 +48,6 @@
             </div>
           </div>
         </div>
-        <!-- Flow arrows -->
-        <div class="flow-arrows">
-          <div class="flow-arrow" v-for="i in 3" :key="i">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <path d="M12 5v14M5 12l7 7 7-7"/>
-            </svg>
-          </div>
-        </div>
       </div>
 
       <!-- Dependency explanation -->
