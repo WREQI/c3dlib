a0e7672f4e71e6394fa51a7cc9e3dcd6d3521d52
asdass <35467304+WREQI@users.noreply.github.com>
2026-08-10 23:55:54 +0800

    1
    1

---
+++ c3dlabs.ru/doc/navtreedata.js
@@ -0,0 +1,221 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "C3D Toolkit", "index.html", [
+    [ "Список устаревших определений и описаний", "deprecated.html", null ],
+    [ "Группы", "modules.html", "modules" ],
+    [ "Члены пространств имен", "namespacemembers.html", [
+      [ "Указатель", "namespacemembers.html", "namespacemembers_dup" ],
+      [ "Функции", "namespacemembers_func.html", null ],
+      [ "Переменные", "namespacemembers_vars.html", null ],
+      [ "Определения типов", "namespacemembers_type.html", null ],
+      [ "Перечисления", "namespacemembers_enum.html", null ],
+      [ "Элементы перечислений", "namespacemembers_eval.html", null ]
+    ] ],
+    [ "Классы", "annotated.html", [
+      [ "Классы", "annotated.html", "annotated_dup" ],
+      [ "Алфавитный указатель классов", "classes.html", null ],
+      [ "Иерархия классов", "hierarchy.html", "hierarchy" ],
+      [ "Члены классов", "functions.html", [
+        [ "Указатель", "functions.html", "functions_dup" ],
+        [ "Функции", "functions_func.html", "functions_func" ],
+        [ "Переменные", "functions_vars.html", "functions_vars" ],
+        [ "Определения типов", "functions_type.html", null ],
+        [ "Перечисления", "functions_enum.html", null ],
+        [ "Элементы перечислений", "functions_eval.html", "functions_eval" ],
+        [ "Относящиеся к классу функции", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Файлы", "files.html", [
+      [ "Файлы", "files.html", "files_dup" ],
+      [ "Список членов всех файлов", "globals.html", [
+        [ "Указатель", "globals.html", "globals_dup" ],
+        [ "Функции", "globals_func.html", "globals_func" ],
+        [ "Переменные", "globals_vars.html", null ],
+        [ "Определения типов", "globals_type.html", null ],
+        [ "Перечисления", "globals_enum.html", null ],
+        [ "Элементы перечислений", "globals_eval.html", "globals_eval" ],
+        [ "Макросы", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"action__analysis_8h.html",
+"class_assembly_m_i_attire.html#ae5e8a633cb16d084e4d21e196bad6a2d",
+"class_balance_tree_node.html#ae173611d7941ffbdfe0c75b5d8376b8c",
+"class_conv_convertor_property3_d.html#a70d958fa832dd49dc4d6f13cb303896f",
+"class_fasteners_values.html#a6eba761febab15077d6f0e0ca31c919e",
+"class_if_draw_g_i.html#afc17c518d6495cfab5db36b9f46500b6",
+"class_ma_annotation_item.html#a13b81837de88da711a3e63e9c57371ed",
+"class_mb_anchor_attribute.html#a85c13325dae6618c8a162490619d78e7",
+"class_mb_arc.html#ad9e0285f17a972c3b34792ca600c64c3",
+"class_mb_attribute.html#a259be243d0b4c5cf08469ee8b0b15649",
+"class_mb_base_vestige.html#aac1cde9668627349bf48a3a07721ccd4a74f5136dda3e62f3e1b379cc850936d8",
+"class_mb_bezier3_d.html#adf5062a703040c96b0d1958afe108ba9",
+"class_mb_cart_point.html#acc1c2f76db6a7b2f5163d44878caa41b",
+"class_mb_character_curve3_d.html#a2a242d9aa2d30c2af2d6dc726f221418",
+"class_mb_composite_function.html#a178d7a0a88098c9c97d67fdc00580348",
+"class_mb_cone_surface.html#af980521c8b00abd80617ea6a10c066b9",
+"class_mb_contour3_d.html#a23148a05cd09826c1f2d9603bfaa0f17",
+"class_mb_contour_on_surface.html#ae658794cbbec02540a9f9764120b7345",
+"class_mb_corner_surface.html#ada8d87bfaf4485b6250a2719cef45f2a",
+"class_mb_cross_point.html#ae63c94443e31eaf20a79c90625a97502",
+"class_mb_cubic_spline.html#ad78c50d8817c33c618aad732f5dba088",
+"class_mb_curve3_d.html#a172b3d5e6e5b1663b4e01ebf3fd0d1dd",
+"class_mb_curve_bounded_surface.html#ae7463ed0d32904a286a08c357c501f3c",
+"class_mb_curve_into_nurbs_info.html#a195efd9955988c0d6f65b780f291ef28",
+"class_mb_cut_solid_array_by_borders_params.html#af76633fb7e67e35e91f5f78878e48005",
+"class_mb_divide_shell_params.html#a03a642d501b3475460ad7276c33e9f2f",
+"class_mb_edge.html#acef8a35bf3d53b1ffdb571385b341063",
+"class_mb_evolution_shell_params.html#a0309e666c8e1a8b86492707878496b3d",
+"class_mb_exaction_surface.html#a145c9f2dafa410adf235922054308552",
+"class_mb_extrusion_surface.html#a730f5aa1835a01efe6996b5f1e05f53a",
+"class_mb_face_shell.html#a565ff7b30dfeb940b91aa4df48f778f3",
+"class_mb_fillet_surface.html#a23889de80798c828eff573f45c805ed3",
+"class_mb_float_grid.html#a49728ba3fe588eb79a5a83046b2b5db9",
+"class_mb_function.html#ab74e9b1cbe0134ee6d16eca0b0ec49c5",
+"class_mb_grid_loop.html",
+"class_mb_halfedge_attr_point3_d.html#a75595230c880e62bfb091fc0f353170a",
+"class_mb_hole_solid_params.html",
+"class_mb_int_curve_creator.html#a4e6f19e4148d4974e0d15c56a840784c",
+"class_mb_join_surface.html#abe320de4b018fa13e3b81a9dc0c01774",
+"class_mb_line_function.html#a869d775e5de701c7bf8b881ee7286fae",
+"class_mb_lofted_surface.html#a3b98701fb0555c91fa34ae32b04f08ed",
+"class_mb_matrix.html#a5f992dfe3fea0d8197c51c0399857a28",
+"class_mb_mesh.html#a8185eb38594a71cfaf1c10369bf41827",
+"class_mb_mesh_unwrap_params.html#a36831d6cfd22bd5d4dd79e3a7e0885be",
+"class_mb_mono_smooth_function.html#a784f9f68e42270f0916b2326d8ea3ca5",
+"class_mb_name_maker.html#a022641f9bc3831ec5e4f46bafb29b478",
+"class_mb_nurbs.html#aa1b2820858a79edb59068309a220b843",
+"class_mb_nurbs_function.html#abd07c8711d8d214715aecc1357640065",
+"class_mb_offset_curve3_d.html#a9d38cd795d7bf260c9fa52e67cf05cff",
+"class_mb_oriented_edge.html#afd09810858ee11512c37723a61103f32",
+"class_mb_placement.html#a763ef09bddd8dbe8b7bf982bde6dce59",
+"class_mb_plane.html#aa06b64a196fabb88704cd479717a1896",
+"class_mb_point3_d.html#a29f1f3afc56f97c6dd27efc71b5be543",
+"class_mb_poly_curve3_d.html#abc4f1b739a8cfa62f739c7afe130c217",
+"class_mb_polyline3_d.html#ac62c8b3b0ea214daeeea3f32dabb2868",
+"class_mb_power_function.html#a43cb9838f29324155dfe37478efe864f",
+"class_mb_property.html#abdd162d985b08743348ddb571eff690c",
+"class_mb_rect2_d.html#a908054eb9bc1f80ed0612e586425bb62",
+"class_mb_reparam_curve.html#a9c3dc4940a239aebc10bdf6c247a52ab",
+"class_mb_revolution_surface.html#a87b053917faac21fe93abbe0c1a43a6d",
+"class_mb_ruled_surface.html#af80b20e63f74cdde900e4fa018edc845",
+"class_mb_section_rail.html#afff270a1bc77a820ecacc9e74ba067bb",
+"class_mb_sector_surface.html#af87a1a5657c355347f661acb1872fd40",
+"class_mb_shell_cutting_params.html#abfd669969302bce96d31d07246b5bd82",
+"class_mb_simple_mesh_creator.html#a89654741453ccd4d56f2a9af8328595a",
+"class_mb_solid.html#a85f05a8d94b8116330108c96ddfd932f",
+"class_mb_sphere_surface.html#aa9154e09b11fbd30ebd5e4c717e7c35b",
+"class_mb_spiral_surface.html#a87c7730dbe33be145e8e3b4158b00471",
+"class_mb_split_shell.html#a660723185f9e76fe9033f6a8c7b97923",
+"class_mb_surface.html#a4a3025c11f2bb8a2c2f8a3583daccaf0",
+"class_mb_surface_fit_constraint.html#a7a7a83b3d8991e6a555c36fecebc69fd",
+"class_mb_surfaces_joint_analysis_params.html#aa811b8545ab14a255437b62d0fa2178b",
+"class_mb_text_literal.html#ab5deb08392aafc3dc0a59c57856287ee",
+"class_mb_transactions.html#a09267fd5debc1f65f80020312b508e77",
+"class_mb_trimmed_curve3_d.html#a6ceaa2c79499f5fac0d93b147b23293d",
+"class_mb_user_func.html#a8d89cd79beea21a41de7a1a1d2250d60",
+"class_mb_vertex.html#a60b7b3b59b2728d7a6cad1cb1c2681ce",
+"class_mb_wire_frame_proj_params.html#a6806edaf34b0dd70d27bb900a5ba410d",
+"class_mp_vertex.html#ac70bdc9f04df1ccd413bff9912ef8e61",
+"class_r_p_array.html#a06fc9da15b4a174a86123dd3ca62da4f",
+"class_scoped_recursive_lock.html",
+"class_tape_registrator.html#ac2c6353baf7da3f3b5c9ea07d919d5f9",
+"class_v_s_n_1_1_box_f.html#a0111490c2598eb500472f3e6eee31439",
+"class_v_s_n_1_1_coord_tuning.html#a1aaab07cbd28fbd09ff9006b16319b79",
+"class_v_s_n_1_1_fields.html#a0683b9a7662842358835e717205bf66e",
+"class_v_s_n_1_1_graphics_view.html#aea3ca27e27d5b8bc3599d70bd421ad96",
+"class_v_s_n_1_1_light_editors_dispatcher.html#a29181e2bd7a7480f4892c8349eb8d949",
+"class_v_s_n_1_1_math_sketch_geometry.html#ad8e27fbe7fa14aa3f1e3edf0a2574fa0",
+"class_v_s_n_1_1_object_pick_selection.html#a90a8bd53db5d507368cbdb9b7c88abee",
+"class_v_s_n_1_1_point_cloud_geometry.html#a980b499c326989e8e559ffb7730378e9",
+"class_v_s_n_1_1_process_event.html#a1cfb5e81558bbb64a63975cedf5385bca719c32354c4753e3a61dcef4d83ba246",
+"class_v_s_n_1_1_render_state.html#a616594205a0147b1696660bb72454ccd",
+"class_v_s_n_1_1_segment_instance.html#a9c405234ab7cd353718e1f704c89a885",
+"class_v_s_n_1_1_string.html#aac02e7f0f7c92c7962e57b8e07707b02",
+"class_v_s_n_1_1_variant.html#a76fbd63fe7ff3243ba71d3b8f9d07f38",
+"class_v_s_n_1_1_widget_model.html#a7caf328fd0d4330d424a32c4a138af8a",
+"classc3d_1_1converter_1_1_se_b_spline_with_knots_curve.html#a5fdef72a02e672aee3068e784e82d274",
+"classiobuf___seq.html#a839ba703c688fc635f28d976d6cdf8eb",
+"conv__annotation__item_8h.html#a6df04dffdcea89fcf91879b53fa3210ea0bc9d836514bcb4c111d8274f8ead436",
+"conv__exchange__settings_8h.html#af19c2431f8b77b7bc8ba3067047cffb0",
+"cur__character__curve3d_8h.html",
+"globals_func_t.html",
+"group___algorithms__3_d.html#ga20802da9a5276ec08899a0624affdde9",
+"group___algorithms__3_d.html#gae51bc37d545359effd0f76b135fb6c9f",
+"group___base___items.html#ggad298d2af79e3480e59c381ffb111b0bca120b62a8d04b6525358b51df97e2f384",
+"group___base___items.html#ggad298d2af79e3480e59c381ffb111b0bca98054285c6979abe111bbd0da42aabab",
+"group___base___tools.html#ga1bac1c4e76d2b7da63f8613649998638",
+"group___base___tools.html#gac883151adfd9f5d39ff1c3eaebd62303",
+"group___base___tools___i_o.html#ga842eb94320b5157c5ca44ebb6723c803",
+"group___collision___detection.html#ggacc090e702a720330783e649c03f4bdf8a2bee729b79b36b726951cca0509e9903",
+"group___constraints2_d___a_p_i.html#ggaa91249a081abda3f538d364aa575d971a6273fba9524c55d7df783995dd6fb55b",
+"group___curve___modeling.html#ga11ba90ba5e63735dcf40e9352f2742cd",
+"group___curves__3_d.html#ga7f490088375a4328ac4392f63fe6ed61",
+"group___data___structures.html#gga0bfcf5a732a70476e7ef1df51ceb3841a64f5bf904d29786f565706136eff3f8f",
+"group___data___structures.html#gga8779515558c436a0cf818cbd800d503baba0b003a61b3facfbe9d1c5037f31423",
+"group___direct___building___parameters.html#gga0f9c5ed5adb38916743aad41f641f6b6a9a1dda182e205e13e3ba2c47cce6b472",
+"group___g_c_m__3_d___a_p_i.html#ga21c354314d0c4e7fc43a3ea4441c35fc",
+"group___g_c_m__3_d___a_p_i.html#gga7551eabba894d4b407dd9955ee4c919fa3c6de083fc1e25c6da99f699f34842da",
+"group___geometric___items.html#gga1cd2ac4649aedd202e93126694b811e4a4b4c892ee0602ec297a3da2b700a299c",
+"group___geometric___items.html#ggafd7eb7897408c4c38de7860da15d876ba64bf11d42795bde4b1385550487c5d02",
+"group___legend.html#ggaae03eb309f4ac495da076d740f4cd3bba791058459e543bd5ca18cb652a369767",
+"group___model___attributes.html#gga2b1a66ae5775e1d0d3e327b114a7667ea88bcd3fd088d5befbbe5d2efa16d60bf",
+"group___model___creators.html#gga96997648ca8bffbf248323cfa514c2bca3680969f6cb76de9c6832c0cd80ea089",
+"group___parasolid___exchange.html#ga31cea3c62053dd87d2b0b2312c3b9f04",
+"group___polygonal___objects.html#ga466c0b2d4c1d67e34737543a5220405d",
+"group___sheet___metal___building___parameters.html#gga3cfe1117e5fafa094f02e35def51bc7ca9c79bc13a93f7c9aef7ef1e5321c7996",
+"group___shell___modeling.html#ga0187d6572543998933334f78a0e63405",
+"group___surface___modeling.html#gab40cfba128653853bb77f0574a356efd",
+"group___vision___process.html#ggaa2c59d589c2d58655f1c656676a58228a1f71c2d2d99092b66a2cdaac79535ae8",
+"mb__pmi_8h.html#ad21be2f0e4365a36cf676ac8a02d4b94",
+"mb__property__title_8h.html#aff36a02591ff535f7cab841a4b0dd312a3880fcca59a5cce41f6281633b26e44a",
+"mb__property__title_8h.html#aff36a02591ff535f7cab841a4b0dd312a739ae253845422300620fdb3ace7d2ae",
+"mb__property__title_8h.html#aff36a02591ff535f7cab841a4b0dd312aa9c97388ab3df8c99b06bf2e6ccb3bab",
+"mb__property__title_8h.html#aff36a02591ff535f7cab841a4b0dd312adbc1ef0c77115d16097ca6dc5bc993c0",
+"mb__variables_8h.html#a593f7744f6001143063cbe1f706d2806",
+"namespacemembers_c.html",
+"struct_active_vertex_segm_const_iterator.html#a6172e839071b189726faa4a63726c883",
+"struct_g_c_e__dragging__point.html",
+"struct_join_surface_values.html#abf22e5fa7fcf6b50d3b8c4e09e22799e",
+"struct_mb_control_data.html#a0b30fb1e3e4f05df12eeba867c25f2ca",
+"struct_mb_fair_create_data.html#aa004606ec348acaa63cc5e1e82ad1ec5",
+"struct_mb_lump.html#a7352d300fdcd3487ba8db0d041e8b92f",
+"struct_mb_section_map_iterator_abs.html",
+"struct_mb_swept_layout.html#a41abe2ae8aeceebcd9baec5e9454844eac92d6027caba4801e8fcdb67f2328607",
+"struct_modify_values.html#ad5d24ce8e1b06bcbf2ee032b45faba1a",
+"struct_smooth_values.html#a2f9e18eb9277acd425c57521ebd74f5e",
+"structc3d__conv__user_1_1_ob_arc3_d.html",
+"tool__log_8h.html#a32d9bab467208f798869d7901baf7bec",
+"vsn__statisticsposition_8h.html#a7187a0809e4dd64007c6319c25a47a54"
+];
+
+var SYNCONMSG = 'нажмите на выключить для синхронизации панелей';
+var SYNCOFFMSG = 'нажмите на включить для синхронизации панелей';
---
+++ c3dlabs.ru/doc/search/searchdata.js
@@ -0,0 +1,51 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghijklmnopqrstuvwxyz~АБВГДИКМОПРСТ",
+  1: "abcdefghijklmnoprstuvw",
+  2: "cv",
+  3: "acfghilmnoprstvw",
+  4: "_abcdefghijklmnopqrstuvwxyz~",
+  5: "_abcdefghijklmnopqrstuvwxyz",
+  6: "abcdefgilmnoprstuv",
+  7: "abcdefghijklmoprstvw",
+  8: "abcdefghijklmnopqrstuvwxz",
+  9: "acefgmorw",
+  10: "_cdelms",
+  11: "acdipsvАБВГДИКМОПРСТ",
+  12: "С"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "related",
+  10: "defines",
+  11: "groups",
+  12: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Пространства имен",
+  3: "Файлы",
+  4: "Функции",
+  5: "Переменные",
+  6: "Определения типов",
+  7: "Перечисления",
+  8: "Элементы перечислений",
+  9: "Друзья",
+  10: "Макросы",
+  11: "Группы",
+  12: "Страницы"
+};
+
